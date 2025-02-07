13fcd81bc6bfaa0e9959a5b689d808eaeaa47c98
Thomas Mol <s130286@ap.be>
2025-02-07 20:56:48 +0100

    Add ScrollToTop component to reset scroll position on route change

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { AnimatePresence } from "framer-motion";
 import { Outlet } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
 import styles from "./App.module.css";
+import ScrollToTop from "./utils/ScrollToTop";
 
 const App = () => {
   return (
     <>
+      <ScrollToTop />
       <div className={styles.main}>
         <div className={styles.header}>
           <h1>
